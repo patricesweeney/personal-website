7f17ce08bf2a2bcced54bb6fd77766b4ec96471c
Patrick Sweeney <patricksweeney018@gmail.com>
2025-12-21 00:50:21 +1100

    refactor: move DemosOverview to features/ for proper vertical slicing
    - Extract DemosOverview component to features/demos/components/
    - Make app/(demos)/demos/page.tsx a thin waiter with metadata export
    - Add DemosOverview to barrel export

--- features/demos/index.ts
+++ features/demos/index.ts
@@ -2,4 +2,5 @@ export { UploadDemo } from './components/UploadDemo'
 export { DemosView } from './components/DemosView'
 export { DemosSidebar } from './components/DemosSidebar'
 export { AnalysisPage } from './components/AnalysisPage'
+export { DemosOverview } from './components/DemosOverview'
 
